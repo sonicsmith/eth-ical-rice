78cd23deae33b4a475b79acdcb8c665f6dab65c7
sonicsmith <thesonicsmith@gmail.com>
2025-02-01 08:53:35 +1300

    Add assets

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ETH-ical Rice
+
+Agentic Ethereum Hackathon Entry
+
+Credits:
+
+- Assets from Sprout Lands -By : Cup Nooble
---
+++ public/assets/grass.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1.1" name="grass" tilewidth="16" tileheight="16" tilecount="77" columns="11">
+ <image source="grass.png" width="176" height="112"/>
+ <tile id="55" probability="0.1"/>
+ <tile id="56" probability="0.1"/>
+ <tile id="57" probability="0.1"/>
+ <tile id="58" probability="0.1"/>
+ <tile id="59" probability="0.1"/>
+ <tile id="60" probability="0.1"/>
+ <tile id="66" probability="0.1"/>
+ <tile id="67" probability="0.1"/>
+ <tile id="68" probability="0.1"/>
+ <tile id="69" probability="0.1"/>
+ <tile id="70" probability="0.1"/>
+ <tile id="71" probability="0.1"/>
+</tileset>
