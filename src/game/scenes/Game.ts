84a5867c1a811ad8dbaf4b3eb97a4ba2171ff726
sonicsmith <thesonicsmith@gmail.com>
2025-02-01 11:56:21 +1300

    Make agent walk to chat partner

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -2,7 +2,7 @@ import { EventBus } from "../EventBus";
 import { Scene } from "phaser";
 import { Character } from "../objects/Character";
 import { agentKeys } from "@/constants";
-import { Script } from "@/types";
+import { AgentInstruction, Script } from "@/types";
 
 export class Game extends Scene {
   mapLayer: Phaser.Tilemaps.TilemapLayer;
@@ -14,6 +14,11 @@ export class Game extends Scene {
     rice: 0,
   };
   riceSupply = 0;
+  agentInstructions: AgentInstruction[] = [];
+  agentInstructionsIndex: number = 0;
+  personInNeed: string = "";
+  foodNeeded: string = "";
+  amountNeeded: number = 0;
 
   constructor() {
     super("Game");
@@ -50,14 +55,16 @@ export class Game extends Scene {
     this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
 
     // Agents
-    const agentBoundsPadding = 100;
-    const agentBoundsX = mapLayer.width - agentBoundsPadding * 2;
-    const agentBoundsY = mapLayer.height - agentBoundsPadding * 2;
-    agentKeys.forEach((key) => {
+    // const agentBoundsPadding = 100;
+    // const agentBoundsX = mapLayer.width - agentBoundsPadding * 2;
+    // const agentBoundsY = mapLayer.height - agentBoundsPadding * 2;
+    agentKeys.forEach((key, index) => {
       const agent = new Character({
         scene: this,
-        x: Math.random() * agentBoundsX + agentBoundsPadding,
-        y: Math.random() * agentBoundsY + agentBoundsPadding,
+        // x: Math.random() * agentBoundsX + agentBoundsPadding,
+        // y: Math.random() * agentBoundsY + agentBoundsPadding,
+        x: mapLayer.width / 2 + (index + 1) * 100,
+        y: mapLayer.height / 2,
         key,
       });
       agent.play(`${key}-idle-down-anim`);
@@ -101,10 +108,32 @@ export class Game extends Scene {
   }
 
   setTodaysScript(script: Script) {
-    console.log("Today's script is", script);
+    this.agentInstructions = script.agentInstructions;
+    this.personInNeed = script.personInNeed;
+    this.amountNeeded = script.amount;
+    this.foodNeeded = script.foodType;
+    this.updateAgents();
+  }
+
+  updateAgents() {
+    const currentIndex = this.agentInstructionsIndex;
+    this.agentInstructionsIndex++;
+    if (currentIndex >= this.agentInstructions.length) {
+      return;
+    }
+    const instructions = this.agentInstructions[currentIndex];
+    const { talker, listener, message } = instructions;
+    const talkerAgent = this.agents[talker];
+    const listenerAgent = this.agents[listener];
+    talkerAgent.setDestination({ x: listenerAgent.x, y: listenerAgent.y });
   }
 
   update(time: number, delta: number) {
+    // Update player
     this.player.update(delta);
+    // Update agents
+    Object.values(this.agents).forEach((agent) => {
+      agent.update(delta);
+    });
   }
 }
