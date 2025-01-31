41b4646eacc37374e0a4762a50b69a5d4e49304f
sonicsmith <thesonicsmith@gmail.com>
2025-02-01 12:32:18 +1300

    Replace hard code with ai script

--- src/app/api/script/generateScript.ts
+++ src/app/api/script/generateScript.ts
@@ -1,3 +1,4 @@
+import { AgentInstruction, Script } from "@/types";
 import OpenAI from "openai";
 
 if (!process.env.DEEP_SEEK_API_KEY) {
@@ -9,12 +10,29 @@ const openai = new OpenAI({
   apiKey: process.env.DEEP_SEEK_API_KEY,
 });
 
-export const generateScript = async () => {
+const context =
+  "You are an NPC in a video game. There are 5 characters in the game." +
+  "Alice, a positive hardworker, with a smile on her face." +
+  "Bob, a grumpy man who has no time for anyone." +
+  "Charlie, a quirky, generous, joker." +
+  "David, a social extrovert who likes to talk." +
+  "Eve, an introverted shy old woman.";
+
+const messageContent =
+  "You are Alice, talking to Bob about how you need 3 lots of wheat.";
+
+const formatting =
+  "Keep responses to 2 sentences or less." +
+  "Do not add any markdown formatting characters.";
+
+export const generateScript = async (): Promise<Script> => {
+  const prompt = `${context} ${messageContent} ${formatting}`;
+
   const completion = await openai.chat.completions.create({
     messages: [
       {
         role: "system",
-        content: "You are an NPC in a farming game. Say hello",
+        content: prompt,
       },
     ],
     model: "deepseek-chat",
@@ -26,5 +44,18 @@ export const generateScript = async () => {
     throw new Error("No text returned from OpenAI");
   }
 
-  return text;
+  const command = {
+    talker: "alice",
+    listener: "bob",
+    message: text,
+  };
+
+  const agentInstructions: AgentInstruction[] = [command];
+
+  return {
+    agentInstructions,
+    personInNeed: "alice",
+    amount: 3,
+    foodType: "wheat",
+  };
 };
--- src/app/api/script/route.ts
+++ src/app/api/script/route.ts
@@ -1,29 +1,10 @@
-import { Script } from "@/types";
 import { NextResponse } from "next/server";
 import { generateScript } from "./generateScript";
 
 export const GET = async () => {
-  const script: Script = {
-    agentInstructions: [
-      {
-        talker: "alice",
-        listener: "bob",
-        message: "Hi bob, do you have any 3 wheat to spare?",
-      },
-      {
-        talker: "bob",
-        listener: "alice",
-        message: "I'm sorry I don't alice",
-      },
-    ],
-    personInNeed: "alice",
-    amount: 3,
-    foodType: "wheat",
-  };
+  const script = await generateScript();
 
-  const aiScript = await generateScript();
-
-  console.log(aiScript);
+  console.log(script);
 
   return NextResponse.json(script);
 };
