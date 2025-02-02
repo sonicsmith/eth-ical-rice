69f938e779ad0546a87e6a46bf0255f87bd17d94
sonicsmith <thesonicsmith@gmail.com>
2025-02-02 18:49:55 +1300

    Add stub for setting farm plots

--- src/hooks/useFarmPlots.ts
+++ src/hooks/useFarmPlots.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export const useFarmPlots = () => {
+export const useFarmPlots = ({ address }: { address: string }) => {
   const [farmPlots, setFarmPlots] = useState<string[]>([]);
 
   return farmPlots;
---
+++ src/utils/setFarmPlot.ts
@@ -0,0 +1,9 @@
+export const setFarmPlots = async ({
+  address,
+  index,
+}: {
+  address: string;
+  index: number;
+}) => {
+  // TODO: Set the farm plot in the contract
+};
