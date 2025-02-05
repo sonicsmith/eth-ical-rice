3e89fb19228c53920263eaa1b2a7a4030f3c7f95
sonicsmith <thesonicsmith@gmail.com>
2025-02-06 12:40:30 +1300

    Improve PublisherView UX

--- src/components/PublisherView.tsx
+++ src/components/PublisherView.tsx
@@ -122,22 +122,26 @@ export const PublisherView = () => {
   return (
     <div className="text-white flex justify-center p-8">
       <div className={"flex flex-col gap-2"}>
-        <div className="text-2xl">Campaign Name:</div>
-        <div>
-          <Input
-            placeholder={"ETHGlobal"}
-            value={campaignName}
-            onChange={(e) => setCampaignName(e.target.value)}
-          />
-        </div>
-        <div className="text-2xl">Campaign Description:</div>
-        <div>
-          <Input
-            placeholder={"Building the most valuable community in web3."}
-            value={campaignDescription}
-            onChange={(e) => setCampaignDescription(e.target.value)}
-          />
-        </div>
+        {hasApproval && (
+          <div className="flex flex-col gap-2">
+            <div className="text-2xl">Campaign Name:</div>
+            <div>
+              <Input
+                placeholder={"ETHGlobal"}
+                value={campaignName}
+                onChange={(e) => setCampaignName(e.target.value)}
+              />
+            </div>
+            <div className="text-2xl">Campaign Description:</div>
+            <div>
+              <Input
+                placeholder={"Building the most valuable community in web3."}
+                value={campaignDescription}
+                onChange={(e) => setCampaignDescription(e.target.value)}
+              />
+            </div>
+          </div>
+        )}
         <div className="text-2xl">Spend Amount ($USD):</div>
         <div>
           <Input
