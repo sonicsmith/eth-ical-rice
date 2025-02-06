8e7950653f1ec7bc5382e1bb98635b9dc5b45fdb
sonicsmith <thesonicsmith@gmail.com>
2025-02-07 09:02:58 +1300

    Add image to login screen

--- src/components/LoginView.tsx
+++ src/components/LoginView.tsx
@@ -3,6 +3,7 @@
 import { usePrivy } from "@privy-io/react-auth";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import Image from "next/image";
 
 export const LoginView = () => {
   const { ready, authenticated, login } = usePrivy();
@@ -10,20 +11,26 @@ export const LoginView = () => {
   const disableLogin = !ready || (ready && authenticated);
 
   return (
-    <div>
+    <div className="bg-[#40722A] p-16">
+      <div className="flex justify-center">
+        <Image src={"/assets/logo.jpg"} width={256} height={256} alt={"logo"} />
+      </div>
       {authenticated ? (
-        <div className="flex justify-center p-32 text-white">
+        <div className="flex justify-center p-8 text-white">
           <div className="flex flex-col gap-8">
-            <Link href={"/game"} className="text-3xl text-center font-bold">
+            <Link
+              href={"/game"}
+              className="text-3xl text-center font-bold underline"
+            >
               Play Game!
             </Link>
             <Link className="text-center" href={"/publish"}>
-              (Publishers click here)
+              (Advertisers click here)
             </Link>
           </div>
         </div>
       ) : (
-        <div className="flex justify-center p-32">
+        <div className="flex justify-center p-8">
           <Button disabled={disableLogin} onClick={login}>
             Login
           </Button>
